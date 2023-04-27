bf73c21cc9a8915ca4f570c8651e1b87b840d9e5
mackow1 <115844078+mackow1@users.noreply.github.com>
2023-04-27 14:21:47 +0200

    replacing 3 files

---
+++ src/main/java/org/battleship/Main.java
@@ -0,0 +1,23 @@
+package org.battleship;
+
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.swing.*;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting application!");
+        Settings settings = new Settings();
+        try {
+            UIManager.setLookAndFeel(new FlatDarkPurpleIJTheme());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+        StartWindow startWindow = new StartWindow(settings);
+    }
+}
